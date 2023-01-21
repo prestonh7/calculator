4a2c70b675961be8e932ed227735be143d412ac0
Preston <PrestonHouse7@gmail.com>
2023-01-21 14:31:00 -0600

    Basic math

---
+++ script.js
@@ -0,0 +1,18 @@
+let firstNumber = 0;
+let secondNumber = 0;
+
+function add(a, b) {
+    return a += b;
+}
+
+function subtract(a, b) {
+    return a -= b;
+}
+
+function multiply(a, b) {
+    return a *= b;
+}
+
+function divide(a, b) {
+    return a /= b;
+}
